9c8354e22c852e79fa69e258845e58b8f7028f89
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-07-14 13:00:46 +0300

    Update Generating.jsx

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -7,8 +7,8 @@ const Generating = ({ className }) => {
         className || ""
       } text-base`}
     >
-      <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
-      AI is generating
+      <img className="w-5 h-5 mr-4" src={loading} alt="Loading......" />
+      AI Is Generating
     </div>
   );
 };
